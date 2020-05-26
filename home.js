2434ea45c097cb289fc45b1c2f341a6117d046bc
Rainhunter13 <rakhmandva@gmail.com>
2020-05-27 04:02:53 +0600

    added menu to home, connected with firebase, organized structure

--- home.js
+++ home.js
@@ -1,6 +1,26 @@
+// FIREBASE CONFIGURATION
+var firebaseConfig = {
+  apiKey: "AIzaSyDlsoAqDJA213Z40hLcSLcDOYuHeMphUWE",
+  authDomain: "dp4heat.firebaseapp.com",
+  databaseURL: "https://dp4heat.firebaseio.com",
+  projectId: "dp4heat",
+  storageBucket: "dp4heat.appspot.com",
+  messagingSenderId: "75419734917",
+  appId: "1:75419734917:web:fc352a3abda9bc74506aec"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
 $( document ).ready( function() {
 
-  var testText = document.getElementById("testText");
-  testText.innerText = "HOME SWEET HOME";
+  var ref = db.collection("users").doc("test");
+  ref.get().then(function (doc) {
+    data = doc.data();
+    for (key in data) {
+      console.log(key);
+    }
+  })
+
+
 
 });
