e8e4e566f6e0cd1a467e0b5ff01536578d461c70
Rainhunter13 <rakhmandva@gmail.com>
2020-06-03 19:44:09 +0600

    filters implemented, style corrections added

--- home.js
+++ home.js
@@ -10,6 +10,103 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
+// GLOBAL VARIABLES
+
+var city = "Daejeon";
+var therapists = db.collection("therapists").doc(city);
+var tbl;
+var sz;
+
+var fSpecialization = "Specialization";
+var fPrice = "Max. Price";
+var fAgeMin = "0";
+var fAgeMax = "100";
+var fRating = "1";
+
+// FILTERS APPLYING FUNCTION
+async function update() {
+
+  tbl = document.getElementById("tbl");
+  for(var i = tbl.rows.length - 1; i >= 0; i--) {
+      tbl.deleteRow(i);
+  }
+  sz = 0;
+
+  await therapists.get().then(async function (doc) {
+    data = doc.data();
+
+    for (key in data) {
+
+      var f1 = fSpecialization==="Specialization" || fSpecialization===data[key]["info"]["specialization"];
+      var f2 = fPrice==="Max. Price" || parseInt(fPrice)>=parseInt(data[key]["description"]["price"]);
+      var f3 = parseInt(fAgeMin)<=parseInt(data[key]["info"]["age"]) && parseInt(fAgeMax)>=parseInt(data[key]["info"]["age"]);
+      var f4 = parseInt(fRating)<=parseInt(data[key]["info"]["rating"]);
+
+
+      if (f1 && f2 && f3 && f4) {
+        var r = tbl.insertRow(sz);
+        var c = r.insertCell(0);
+        $(c).addClass("cell");
+        if (sz % 2 === 1) c.style.backgroundColor = "white"; else c.style.backgroundColor = "#e4e4e4";
+        c.style.cursor = "pointer";
+
+        var x = parseInt(data[key]["info"]["rating"]);
+        var s = "";
+        for (var i=1; i<=x; i++) {
+          s += "&starf; ";
+        }
+
+        c.innerHTML += "<b style='font-size: 25px'>" + key + ", " + data[key]["info"]["age"] + "</b>";
+        c.innerHTML += "<br>" + data[key]["info"]["specialization"];
+        c.innerHTML += "<br>" + data[key]["description"]["price"] + "$ per hour";
+        c.innerHTML += "<br>" + data[key]["description"]["district"];
+        c.innerHTML += "<br>" + "<b style='font-size: 20px'>" + "" + s + "</b>";
+
+        c.onclick = function () {
+          if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
+            this.style.border = "0px";
+          } else {
+            $(this).addClass("selected");
+            this.style.border = "2px solid blue";
+          }
+        };
+
+        $(c).hover(
+          function () {
+            if (!$(this).hasClass("selected")) this.style.border = "1px solid blue"
+          },
+          function () {
+            if (!$(this).hasClass("selected")) this.style.border = "0px";
+          }
+        );
+
+        sz++;
+      }
+
+    }
+
+    if (tbl.rows.length===0) {
+      var rr = tbl.insertRow(0);
+      var cc = rr.insertCell(0);
+      $(cc).addClass("cell");
+      cc.style.fontSize = "35px";
+      cc.style.height = "450px";
+      cc.style.verticalAlign = "center";
+      cc.style.alignContent = "center";
+      cc.style.textAlign = "center";
+      cc.innerHTML = "We are sorry, no therapists match your parameters :(";
+    }
+
+  });
+
+  var results = document.getElementById("results");
+  results.innerHTML = sz.toString() + " result(s) found in Daejeon";
+
+}
+
+
+// DOCUMENT LOADED MAIN FUNCTION
 $( document ).ready(function() {
 
   // SEARCH BY FILTERS
@@ -17,25 +114,6 @@ $( document ).ready(function() {
   search_icon.onclick = function () {
     alert("works");
   };
-
-  // RATING FILTER CONTROL (STARS SIZE IS A BULL SHIT)
-  var rating = document.getElementById("rating");
-  var r5 = document.getElementById("5stars");
-  var r4 = document.getElementById("4stars");
-  var r3 = document.getElementById("3stars");
-  var r2 = document.getElementById("2stars");
-  var r1 = document.getElementById("r1");
-  $(rating).change(function () {
-    if (r5.selected || r4.selected || r3.selected || r2.selected) {
-      rating.style.paddingBottom = "7.5px";
-      rating.style.fontSize = "20px";
-      rating.style.width = "167.5px";
-    }
-    if (r1.selected) {
-      rating.style.paddingBottom = "2px";
-      rating.style.fontSize = "25px";
-    }
-  });
 
   // CHANGE CITY INFO ICON
   var info = document.getElementById("info");
@@ -53,58 +131,75 @@ $( document ).ready(function() {
     }
   );
 
-  // MAKE THERAPIST LIST TABLE FROM FIREBASE
-  var city = "Daejeon";
-  var therapists = db.collection("therapists").doc(city);
+  // MAKE THERAPIST LIST FROM FIREBASE WHEN REFRESHING
+  update();
 
-  var tbl = document.getElementById("tbl");
-  var sz = 0;
+  // FILTERS EVENT LISTENERS
 
-  (async () => {
+  var specialization = document.getElementById("specialization");
+  $(specialization).change(function () {
+    if (this.value === "Specialization") fSpecialization = "Specialization";
+    if (this.value === "Family Therapy") fSpecialization = "Family Therapist";
+    if (this.value === "Children Therapy") fSpecialization = "Children Therapist";
+    if (this.value === "Stress and Anxiety") fSpecialization = "Stress and Anxiety Therapist";
+    if (this.value === "Career") fSpecialization = "Career Therapist";
+    update();
+  });
 
-    await therapists.get().then(async function (doc) {
-      data = doc.data();
+  var price = document.getElementById("price");
+  $(price).change(function () {
+    if (this.value === "Max. Price") fPrice = "Max. Price";
+    if (this.value === "25$ per hour") fPrice = "25";
+    if (this.value === "50$ per hour") fPrice = "50";
+    if (this.value === "75$ per hour") fPrice = "75";
+    if (this.value === "100$ per hour") fPrice = "100";
+    update();
+  });
 
-      for (key in data) {
+  var age = document.getElementById("age");
+  $(age).change(function () {
+    if (this.value === "Age") {
+      fAgeMin = "0";
+      fAgeMax = "100";
+    }
+    if (this.value === "< 30 years old") {
+      fAgeMin = "0";
+      fAgeMax = "29"
+    }
+    if (this.value === "30-50 years old") {
+      fAgeMin = "30";
+      fAgeMax = "50"
+    }
+    if (this.value === "> 50 years old") {
+      fAgeMin = "51";
+      fAgeMax = "100"
+    }
+    update();
+  });
 
+  var rating = document.getElementById("rating");
+  var r5 = document.getElementById("5stars");
+  var r4 = document.getElementById("4stars");
+  var r3 = document.getElementById("3stars");
+  var r2 = document.getElementById("2stars");
+  var r1 = document.getElementById("r1");
+  $(rating).change(function () {
+    if (this.value === "Min. Rating") fRating = "1";
+    if (this.value.length === 9) fRating = "5";
+    if (this.value.length === 7) fRating = "4";
+    if (this.value.length === 5) fRating = "3";
+    if (this.value.length === 3) fRating = "2";
+    update();
+    if (r5.selected || r4.selected || r3.selected || r2.selected) {
+      rating.style.paddingBottom = "7.5px";
+      rating.style.fontSize = "20px";
+      rating.style.width = "167.5px";
+    }
+    if (r1.selected) {
+      rating.style.paddingBottom = "2px";
+      rating.style.fontSize = "25px";
+    }
+  });
 
-        var r = tbl.insertRow(sz);
-        var c = r.insertCell(0);
-        $(c).addClass("cell");
-        if (sz%2===1) c.style.backgroundColor = "white";
-        c.style.cursor = "pointer";
-
-        c.innerHTML += "<b style='font-size: 20px'>"+key + ", " + data[key]["info"]["age"] +"</b>";
-        c.innerHTML += "<br>" + data[key]["info"]["specialization"];
-        c.innerHTML += "<br>" + data[key]["description"]["price"] + "$ per hour";
-        c.innerHTML += "<br>" + data[key]["description"]["district"];
-
-        c.onclick = function () {
-          alert("clicked");
-        };
-
-        sz++;
-
-
-        // database filling (fake values)
-        // var sp = ["Gung-Dong", "Dunsan-Dong", "Eoeun-Dong", "Doryong-Dong", "Daehwa-Dong", "Samseong-Dong"];
-        // data[key]["description"]["district"] = sp[Math.floor(Math.random()*6)];
-        // therapists.update({
-        //   [key]: data[key]
-        // })
-
-      }
-
-    });
-
-    var note = document.getElementById("note");
-    note.innerHTML = sz.toString() + note.innerHTML;
-
-
-
-
-
-
-  })();
 
 });
